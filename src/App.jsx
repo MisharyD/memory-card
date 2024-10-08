122c9f3486cd8fb47ed6ce72a5e6ad993913b227
thord <masharyahmed123@gmail.com>
2024-10-08 17:58:40 +0300

    plan and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Header from "./components/Header"
+import Card from "./components/Header"
+import src1 from "./assets/images/abyss-watchers.png"
+import src2 from "./assets/images/abyss-watchers.png"
+import src3 from "./assets/images/abyss-watchers.png"
+import src4 from "./assets/images/abyss-watchers.png"
+import src5 from "./assets/images/abyss-watchers.png"
+import src6 from "./assets/images/abyss-watchers.png"
+import src7 from "./assets/images/abyss-watchers.png"
+import src8 from "./assets/images/abyss-watchers.png"
 
 function App() {
-  console.log("test");
 
+  const [images, setImages] = useState([])
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    //load images
+  }, [])
+
+  useEffect(() => {
+    //shuffle images
+  }, [score])
+
+  //updates clicked boolean of image, updates score depending 
+  function handleClick()
+  {
+
+  }
+
+  //header takes score state
+  //render cards and pass src, id, handleclick
   return (
     <>
-      
+      <Header />
+
+      <img src= {src1} alt="" className=''/>
     </>
   )
 }
